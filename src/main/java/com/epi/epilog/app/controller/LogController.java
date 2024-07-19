e1b7d9d789ae2c7d5fd64f0d1643d27f14b04695
ppparkta <pppppark2@gmail.com>
2024-07-19 11:52:00 +0900

    feat/#85: 월별 일지 개수 조회 api 개발

--- src/main/java/com/epi/epilog/app/controller/LogController.java
+++ src/main/java/com/epi/epilog/app/controller/LogController.java
@@ -1,19 +1,54 @@
 package com.epi.epilog.app.controller;
 
+import com.epi.epilog.app.dto.LogsResponseDto;
+import com.epi.epilog.app.service.LogQueryService;
+import com.epi.epilog.global.exception.ApiException;
+import com.epi.epilog.global.exception.ErrorCode;
+import com.epi.epilog.global.utils.CustomUserDetails;
+import com.epi.epilog.global.utils.DateTimeConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.regex.Pattern;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/logs")
 public class LogController {
+    private final LogQueryService logQueryService;
+
     /**
      * 월별 일지 개수 조회
      */
     @GetMapping("/count")
-    public
+    public LogsResponseDto.MonthLogsCount monthLogsCount(@RequestParam(value = "date", required = false) String date){
+        try {
+            LocalDate queryDate;
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !(authentication.getPrincipal() instanceof CustomUserDetails)) {
+                throw new ApiException(ErrorCode.INVALID_TOKEN);
+            }
+
+            if (date == null) {
+                 date  = DateTimeConverter.convertLocalDateToString(LocalDate.now());
+            }
+            queryDate = DateTimeConverter.convertToLocalDate(date);
+
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            LogsResponseDto.MonthLogsCount logsCount = logQueryService.monthLogsCount(queryDate, userDetails.getMember());
+            return logsCount;
+        } catch (Exception e) {
+            throw new ApiException(ErrorCode.INTERNAL_SERVER_ERROR, e);
+        }
+    }
+
     /**
      * 일별 일지 목록 조회
      */
