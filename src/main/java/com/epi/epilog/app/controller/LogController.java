082af6541780541b43543458d52ec2789e44927f
ppparkta <pppppark2@gmail.com>
2024-07-19 10:51:24 +0900

    feat/#85: log controller 및 응답 전용 DTO 추가

---
+++ src/main/java/com/epi/epilog/app/controller/LogController.java
@@ -0,0 +1,49 @@
+package com.epi.epilog.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/logs")
+public class LogController {
+    /**
+     * 월별 일지 개수 조회
+     */
+    @GetMapping("/count")
+    public
+    /**
+     * 일별 일지 목록 조회
+     */
+
+    /**
+     * 일별 평균, 식전후 평균 혈당 조회
+     */
+
+    /**
+     * 일별 혈당 목록 조회
+     */
+
+    /**
+     * 월별 체중, 체지방률 목록 조회
+     */
+
+    /**
+     * 일지 등록
+     */
+
+    /**
+     * 일지 수정
+     */
+
+    /**
+     * 일지 상세 조회
+     */
+
+    /**
+     * PDF 변환하기
+     */
+
+}
---
+++ src/main/java/com/epi/epilog/app/dto/LogsRequestDto.java
@@ -0,0 +1,4 @@
+package com.epi.epilog.app.dto;
+
+public class LogsRequestDto {
+}
