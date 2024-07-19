d1746b804a161a49a45a3d85c14ba495b2b623a2
ppparkta <pppppark2@gmail.com>
2024-07-19 14:00:53 +0900

    feat/#85: 일별 일지 목록 조회 api 개발

--- src/main/java/com/epi/epilog/app/service/LogQueryService.java
+++ src/main/java/com/epi/epilog/app/service/LogQueryService.java
@@ -1,13 +1,18 @@
 package com.epi.epilog.app.service;
 
 import com.epi.epilog.app.domain.log.Log;
+import com.epi.epilog.app.domain.log.LogMood;
 import com.epi.epilog.app.domain.member.Member;
 import com.epi.epilog.app.dto.CustomUserInfoDto;
 import com.epi.epilog.app.dto.LogsResponseDto;
+import com.epi.epilog.app.repository.LogExerciseRepository;
+import com.epi.epilog.app.repository.LogMoodRepository;
 import com.epi.epilog.app.repository.LogRepository;
 import com.epi.epilog.app.repository.MemberRepository;
 import com.epi.epilog.global.exception.ApiException;
 import com.epi.epilog.global.exception.ErrorCode;
+import com.epi.epilog.global.utils.CustomUserDetails;
+import com.epi.epilog.global.utils.DateTimeConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +33,15 @@ import java.util.stream.Collectors;
 public class LogQueryService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
+    private final LogMoodRepository logMoodRepository;
+    private final LogExerciseRepository logExerciseRepository;
+
+    /**
+     * 월별 일지 개수 조회
+     * @param date
+     * @param member
+     * @return
+     */
     public LogsResponseDto.MonthLogsCount monthLogsCount(LocalDate date, CustomUserInfoDto member) {
         Member saveMember = memberRepository.findById(member.getId())
                 .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
@@ -66,5 +81,54 @@ public class LogQueryService {
                         .count(entry.getValue())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 일별 일지 목록 조회
+     * @param member
+     * @param queryDate
+     * @return
+     */
+    public LogsResponseDto.DayLogsList dayLogList(CustomUserDetails member, LocalDate queryDate) {
+        Member saveMember = memberRepository.findById(member.getMember().getId())
+                .orElseThrow(() -> new ApiException(ErrorCode.USER_NOT_FOUND));
+
+        List<LogsResponseDto.DayLogsItem> logs = dayLogItems(saveMember, queryDate);
+
+        return LogsResponseDto.DayLogsList.builder()
+                .date(DateTimeConverter.convertLocalDateToString(queryDate))
+                .count(logs.size())
+                .logs(logs)
+                .build();
+    }
+
+    private List<LogsResponseDto.DayLogsItem> dayLogItems(Member saveMember, LocalDate queryDate) {
+        List<Log> logs = logRepository.findAllByDateAndMember(queryDate, saveMember);
+//        log.info("repo logs="+logs);
+        return logs.stream().map(log ->
+                LogsResponseDto.DayLogsItem
+                        .builder()
+                        .id(log.getId())
+                        .title(log.getTitle())
+                        .keyword(getKeywords(log))
+                        .build()
+        ).collect(Collectors.toList());
+    }
+
+    private List<String> getKeywords(Log log) {
+        if (log == null)
+            return null;
+        List<String> keywordList = new ArrayList<>();
+        if (log.getBloodSugar() != null)
+            keywordList.add("혈당");
+        if (log.getWeight() != null || log.getBodyFatPercentage() != null || log.getBodyPhoto() != null)
+            keywordList.add("몸무게");
+        if (log.getHeartRate() != null || log.getDiastolicBloodPressure() != null || log.getSystolicBloodPressure() != null)
+            keywordList.add("혈압");
+        if (!logMoodRepository.findByLog(log).isEmpty())
+            keywordList.add("기분");
+        if (!logExerciseRepository.findByLog(log).isEmpty())
+            keywordList.add("운동");
+        return keywordList;
     }
 }
