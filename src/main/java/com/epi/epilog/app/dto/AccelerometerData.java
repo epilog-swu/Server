2c24e7b5d19dd51cdbf5014c278dd46dd9094419
ppparkta <pppppark2@gmail.com>
2024-07-23 23:46:39 +0900

    feat/#17: mongoDB 설정

--- src/main/java/com/epi/epilog/app/dto/AccelerometerData.java
+++ src/main/java/com/epi/epilog/app/dto/AccelerometerData.java
@@ -2,9 +2,11 @@ package com.epi.epilog.app.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
+@Document(collection = "sensor_data")
 public class AccelerometerData {
     private double x;
     private double y;
